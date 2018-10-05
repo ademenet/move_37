6d0ab039df796da72b030b8e1bde74488fd410ba
Alain DEMENET <ademenet@e3r1p11.42.fr>
2018-10-05 17:58:04 +0200

    add some more content for week1

---
+++ week1/102_gridworld.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Environment(object):
+    def __init__(self):
+        pass
+    
+    def step(self, action):
+        pass
+    
+    def reset(self):
+        pass
+    
+    def render(self, mode='human', close=False)
+        pass
